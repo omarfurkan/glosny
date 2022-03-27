868ba3fd2acb60990959ee6e9d1cd14f52ac8d4b
omarfurkan <omarfurkan92@gmail.com>
2022-03-27 11:49:27 +0600

    fetch is done with shop compoent

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product></Product>)
+                }
+            </div>
+            <div className='cart-container'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
